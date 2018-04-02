6ec3887e56233ed497259a8279ba147ddc3a215f
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-04-02 17:30:50 +0200

    Cambia clases de los divs para mostrarlos o no

--- starter-code/js/game.js
+++ starter-code/js/game.js
@@ -3,19 +3,25 @@ function Game(container) {
   this.numberOfMoles = 5;
   this.container = container;
   this.html = "";
-
+  this.indexesMole = [];
+  this.domMole;
   this.start();
 }
 
 Game.prototype.start = function() {
   this.generateMoles();
+  this.generateHtml();
+  this.interval = setInterval(function(){
+    this.resetMole()
+    this.getRandomMoles();
+  }.bind(this),2000)
+  
 };
 
 Game.prototype.generateMoles = function() {
   for (var i = 0; i < this.numberOfMoles; i++) {
     this.moleArray.push(new Mole());
   }
-  this.generateHtml();
 };
 
 Game.prototype.generateHtml = function() {
@@ -23,32 +29,37 @@ Game.prototype.generateHtml = function() {
     this.html += '<div class="mole" id="' + index + '"></div>';
   });
   $(this.container).html(html);
-  this.getRandomMoles();
+  this.domMole = $(".mole");
 };
 
 Game.prototype.getRandomMoles = function() {
   var numMoles = Math.floor(Math.random() * 2 + 1);
-  var indexesMole = [];
   for (var i = 0; i < numMoles; i++) {
-    indexesMole.push(
+    this.indexesMole.push(
       this.moleArray.indexOf(
         this.moleArray[Math.floor(Math.random() * this.moleArray.length)]
       )
     );
-    if (indexesMole[0] === indexesMole[1]) {
-      indexesMole.pop();
+    if (this.indexesMole[0] === this.indexesMole[1]) {
+      this.indexesMole.pop();
     } else {
       continue;
     }
   }
-  this.showHide(indexesMole);
+  this.showMole(this.indexesMole);
 };
 
-Game.prototype.showHide = function(indexesMole) {
-  var domElement = $(".mole");
+Game.prototype.showMole = function(indexesMole) {
+  
   indexesMole.forEach(function(mole) {
-    console.log(mole)
-    $(domElement[mole]).addClass('active')
-  });
+    console.log(this.domMole)
+    console.log(this);
+    $(this.domMole[mole]).addClass('active')
+  }.bind(this));
+  this.indexesMole = []
+};
 
+
+Game.prototype.resetMole = function() {
+  $(this.domMole).removeClass('active')
 };
---
+++ starter-code/js/mole.js
@@ -0,0 +1,28 @@
+var Mole = function (x, y){
+    this.x = x;
+    this.y = y;
+    this.live = 3000;
+}
+
+
+
+
+
+
+
+
+
+// Topos.prototype.randomTopo = function(){
+    
+// }
+
+// this.topo = [
+//     {name: "topo1", live: 3000},
+//     {name: "topo2", live: 3000},
+//     {name: "topo3", live: 3000},
+//     {name: "topo4", live: 3000},
+//     {name: "topo5", live: 3000},    
+// ];
+// this.live = 3000;
+// this.clickMole = [];
+// this.randomTopo();

7b45b79e2c57441cc6c41e13220242f538c90adb
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-04-02 09:10:00 +0200

    Creación primeros archivos

---
+++ starter-code/js/game.js
@@ -0,0 +1,18 @@
+function Game() {
+    this.score = ;
+    this.time = ;
+    
+    this.start = ();
+    this.reset();
+    this.gameOver();
+  }
+  
+  Game.prototype.start = function(){
+
+  }
+  Game.prototype.reset = function(){
+
+  }
+  Game.prototype.gameOver = function(){
+    
+  }
---
+++ starter-code/js/mole.js
@@ -0,0 +1,13 @@
+var Topos = function (){
+    this.topo = [
+        {name: "topo1", live: 3000},
+        {name: "topo2", live: 3000},
+        {name: "topo3", live: 3000},
+        {name: "topo4", live: 3000},
+        {name: "topo5", live: 3000},    
+    ];
+    this.live = 3000;
+    this.clickMole = [];
+    this.randomTopo();
+
+}
